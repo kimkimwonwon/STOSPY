16fa4982b55406b244f6b1f4ca5a59742a026224
Won Kim <kw7383@yonsei.ac.kr>
2023-05-17 19:21:12 +0900

    setup fin

--- kafka_python/main.py
+++ kafka_python/main.py
@@ -2,7 +2,7 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 
-topic_name= ''
+topic_name= 'stockpractice'
 
 producer = KafkaProducer(bootstrap_servers =['localhost:9092'],value_serializer = lambda x : dumps(x).encode('utf-8'))
 
