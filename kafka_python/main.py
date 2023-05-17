5a0ba10e260dd3415c3d59fc8929765f395d7027
Won Kim <kw7383@yonsei.ac.kr>
2023-05-17 18:45:05 +0900

    [modify] setup

---
+++ kafka_python/main.py
@@ -0,0 +1,16 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+topic_name= ''
+
+producer = KafkaProducer(bootstrap_servers =['localhost:9092'],value_serializer = lambda x : dumps(x).encode('utf-8'))
+
+for e in range(1000):
+    data= {'number' : e}
+    print(data)
+    producer.send(topic_name , value = data)
+    sleep(5)
+
+if __name__ == "__main__":
+    print("hello")
